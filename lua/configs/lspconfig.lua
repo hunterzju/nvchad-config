78f8a76e6913cb6bb633b3cddc6407f84766d654
hunterzju <hunter_ht@zju.edu.cn>
2024-06-06 23:51:53 +0800

    init my_config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "clangd", "pylsp" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -21,3 +21,26 @@ lspconfig.tsserver.setup {
   on_init = on_init,
   capabilities = capabilities,
 }
+
+-- local lspconfig = require('lspconfig')
+
+-- pylsp
+lspconfig.pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          -- ignore = {'W391'},
+          ignore = {"E221", "E501", "W504", "E302", "E305", "E402", "W391"},
+          maxLineLength = 100
+        }
+      }
+    }
+  }
+}
+
+-- clangd
+lspconfig.clangd.setup {
+  cmd = {"clangd"}
+}
+
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- telescop key bindings 
+map("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "telescope lsp_definitions" })
+map("n", "gr", "<cmd>Telescope lsp_references<CR>", { desc = "telescope lsp_references" })
+map("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "telescope lsp_implementations" })
+
