5b371896664afaa2875d0850f16c748dfe535ae4
hunterzju <hunter_ht@zju.edu.cn>
2025-01-09 23:42:25 +0800

    init my config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "clangd", "pylsp" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -16,9 +16,22 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+-- configuring single server
+-- pylsp config
+lspconfig.pylsp.setup {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {"E221", "E501", "W504", "E302", "E305", "E402", "W391"},
+          maxLineLength = 100
+        }
+      }
+    }
+  }
+}
+
+-- clangd config
+lspconfig.clangd.setup {
+  cmd = {"clangd"}
+}
